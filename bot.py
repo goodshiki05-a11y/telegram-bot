7354ae63bf3d5d712a201f8ea70cbc9cb57f1f55
goodshiki05-a11y <goodshiki05@gmail.com>
2026-03-13 21:07:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@ from telegram.ext import ApplicationBuilder, ChatJoinRequestHandler, ContextType
 
 TOKEN = "8647126204:AAFO5NRWQud8ATjwVh8r30QV0pn_Av6K5so"
 
-VIP_LINK_1 = "https://t.me/+4QZ5jMN6vTczMjAy"
+VIP_LINK = "https://t.me/+4QZ5jMN6vTczMjAy"
 
-VIP_LINK_2 = "https://t.me/+3TAQgBgnShhiZTU6"
 
 
 async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,11 +12,10 @@ async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     text = """𝗦𝗶𝘇𝗻𝗶𝗻𝗴 ᴊᴜꜱᴛ ꜰᴏʀᴇx 𝗞𝗔𝗡𝗔𝗟𝗜𝗚𝗔 𝗾𝗼'𝘀𝗵𝗶𝗹𝗶𝘀𝗵 𝘀𝗼'𝗿𝗼𝘃𝗶𝗻𝗴𝗶𝘇 𝗺𝘂𝘃𝗮𝗳𝗳𝗮𝗾𝗶𝘆𝗮𝘁𝗹𝗶 𝘁𝗮𝘀𝗱𝗶𝗾𝗹𝗮𝗻𝗱𝗶 ✅
 
-🎁 𝗦𝗶𝘇𝗴𝗮 𝗯𝗼𝗻𝘂𝘀 𝘀𝗶𝗳𝗮𝘁𝗶𝗱𝗮 𝗯𝗶𝘇𝗻𝗶𝗻𝗴 𝗩𝗜𝗣 𝘃𝗮 𝗥𝗔𝗭𝗚𝗢𝗡 𝗞𝗔𝗡𝗔𝗟𝗟𝗔𝗥𝗜𝗠𝗜𝗭 𝗾𝗼‘𝘀𝗵𝗶𝗯 𝗯𝗲𝗿𝗶𝗹𝗱𝗶❗️"""
+🎁 𝗦𝗶𝘇𝗴𝗮 𝗯𝗼𝗻𝘂𝘀 𝘀𝗶𝗳𝗮𝘁𝗶𝗱𝗮 𝗯𝗶𝘇𝗻𝗶𝗻𝗴 𝗩𝗜𝗣 𝘃𝗮 𝗥𝗔𝗭𝗚𝗢𝗡 𝗞𝗔𝗡𝗔𝗟𝗜𝗠𝗜𝗭 𝗾𝗼‘𝘀𝗵𝗶𝗯 𝗯𝗲𝗿𝗶𝗹𝗱𝗶❗️"""
 
     keyboard = [
-        [InlineKeyboardButton("VIP KANAL 1", url=VIP_LINK_1)],
-        [InlineKeyboardButton("VIP KANAL 2", url=VIP_LINK_2)]
+        [InlineKeyboardButton("VIP KANAL", url=VIP_LINK)]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
