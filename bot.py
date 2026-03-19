f9cfda13db2efe1656e4c5720d3a6de4ce3b24a8
goodshiki05-a11y <goodshiki05@gmail.com>
2026-03-19 20:09:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,19 @@ TOKEN = "8647126204:AAEg80wQoRjR04HQpNlYyzblbaHbUT50neg"
 
 VIP_LINK_1 = "https://t.me/+c6cK0y1bYHc2M2Qy"
 VIP_LINK_2 = "https://t.me/+3TAQgBgnShhiZTU6"
+VIP_LINK_3 = "https://t.me/+Uf6RWm1E4e1mMTY6"  
 
 async def join(update: Update, context: ContextTypes.DEFAULT_TYPE):
     req = update.chat_join_request
 
-    text = """𝗦𝗶𝘇𝗻𝗶𝗻𝗴 ᴊᴜꜱᴛ ꜰᴏʀᴇx 𝗞𝗔𝗡𝗔𝗟𝗜𝗚𝗔 𝗾𝗼'𝘀𝗵𝗶𝗹𝗶𝘀𝗵 𝘀𝗼'𝗿𝗼𝘃𝗶𝗻𝗴𝗶𝘇 𝗺𝘂𝘃𝗮𝗳𝗳𝗮𝗾𝗶𝘆𝗮𝘁𝗹𝗶 𝘁𝗮𝘀𝗱𝗶𝗾𝗹𝗮𝗻𝗱𝗶 ✅
+    text = """𝗦𝗶𝘇𝗻𝗶𝗻𝗴 𝗧𝗥𝗔𝗗𝗜𝗡𝗚 Kanalimizga 𝗾𝗼'𝘀𝗵𝗶𝗹𝗶𝘀𝗵 𝘀𝗼'𝗿𝗼𝘃𝗶𝗻𝗴𝗶𝘇 𝗺𝘂𝘃𝗮𝗳𝗳𝗮𝗾𝗶𝘆𝗮𝘁𝗹𝗶 𝘁𝗮𝘀𝗱𝗶𝗾𝗹𝗮𝗻𝗱𝗶 ✅
 
 🎁 𝗦𝗶𝘇𝗴𝗮 𝗯𝗼𝗻𝘂𝘀 𝘀𝗶𝗳𝗮𝘁𝗶𝗱𝗮 𝗯𝗶𝘇𝗻𝗶𝗻𝗴 𝗩𝗜𝗣 𝘃𝗮 𝗥𝗔𝗭𝗚𝗢𝗡 𝗞𝗔𝗡𝗔𝗟𝗟𝗔𝗥𝗜𝗠𝗜𝗭 𝗾𝗼‘𝘀𝗵𝗶𝗯 𝗯𝗲𝗿𝗶𝗹𝗱𝗶❗️"""
 
     keyboard = [
         [InlineKeyboardButton("VIP KANAL 1", url=VIP_LINK_1)],
-        [InlineKeyboardButton("VIP KANAL 2", url=VIP_LINK_2)]
+        [InlineKeyboardButton("VIP KANAL 2", url=VIP_LINK_2)],
+        [InlineKeyboardButton("VIP KANAL 3", url=VIP_LINK_3)]  
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
